7740f5aa8a8385ee972ea47046a4cadd635428cf
Rishi Soni <rishisoni.5678@gmail.com>
2021-11-15 21:16:46 -0500

    Add files via upload

---
+++ T-Shirt Cannon/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+
+//Store all port mappings in RobotMap
+
+public final class RobotMap {
+    
+    //Motor Ports
+    final static int M_FRONT_LEFT = 0;
+    final static int M_REAR_LEFT = 0;
+    final static int M_FRONT_RIGHT = 0;
+    final static int M_REAR_RIGHT = 0;
+
+    //Drive Ports
+    final static int JOYSTICK = 0;
+}
