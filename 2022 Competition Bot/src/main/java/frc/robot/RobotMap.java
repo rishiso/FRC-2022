f9e8990163aa59a5c79dcdb9618fdd2e4a4f1c1e
Rishi Soni <rishisoni.5678@gmail.com>
2022-01-20 18:53:45 -0500

    Update RobotMap.java

--- 2022 Competition Bot/src/main/java/frc/robot/RobotMap.java
+++ 2022 Competition Bot/src/main/java/frc/robot/RobotMap.java
@@ -6,10 +6,8 @@ package frc.robot;
 public final class RobotMap {
     
     //Motor Ports
-    final static int M_FRONT_LEFT = 0;
-    final static int M_REAR_LEFT = 0;
-    final static int M_FRONT_RIGHT = 0;
-    final static int M_REAR_RIGHT = 0;
+    final static int M_LEFT = 0;
+    final static int M_RIGHT = 0;
 
     //Drive Ports
     final static int JOYSTICK = 0;
