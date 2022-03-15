88c032191a9e04b847c5a2aa1aeab1c345a481ab
Rishi Soni <rishisoni.5678@gmail.com>
2022-03-15 15:35:58 -0400

    Set PWM mapping for motor controllers

--- 2022 Competition Bot/src/main/java/frc/robot/RobotMap.java
+++ 2022 Competition Bot/src/main/java/frc/robot/RobotMap.java
@@ -7,7 +7,7 @@ public final class RobotMap {
     
     //Motor Ports
     final static int M_LEFT = 0;
-    final static int M_RIGHT = 0;
+    final static int M_RIGHT = 1;
 
     //Drive Ports
     final static int JOYSTICK = 0;
