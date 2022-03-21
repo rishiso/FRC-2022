063c5be273330567991ef354838a4a3face73475
Rishi Soni <rishisoni.5678@gmail.com>
2022-03-21 16:15:26 -0400

    Added Shooting Functionality

--- 2022 Competition Bot/src/main/java/frc/robot/RobotMap.java
+++ 2022 Competition Bot/src/main/java/frc/robot/RobotMap.java
@@ -8,7 +8,12 @@ public final class RobotMap {
     //Motor Ports
     final static int M_LEFT = 0;
     final static int M_RIGHT = 1;
+    final static int M_SHOOTER = 2; 
 
     //Drive Ports
     final static int JOYSTICK = 0;
+
+    //Joystick Buttons
+    final static int SHOOT = 1;
+    final static int RELOAD = 2;
 }
